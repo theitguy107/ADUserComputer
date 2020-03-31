c902e53427888ec2d7099252f8cf6dac1d3d7cb8
Joshua Morden <45412544+theitguy107@users.noreply.github.com>
2020-03-31 15:43:33 -0400

    Add files via upload

---
+++ Enter-ADUserComputer.ps1
@@ -0,0 +1,15 @@
+﻿Function Enter-ADUserComputer
+
+{
+
+param(
+[Parameter(Mandatory)]
+$FullName
+)
+
+
+$Domain = Get-ADDomain | Select-Object -ExpandProperty DNSRoot
+$Computer = get-adcomputer -filter {Description -like $FullName} -properties Description | Select-Object -ExpandProperty Name
+Enter-PSSession "$Computer.$Domain"
+
+}
